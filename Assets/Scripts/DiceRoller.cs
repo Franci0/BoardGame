665926fcc52afccde0bf0b921bf898ee25d09c84
Franci0 <ffranci0.89@gmail.com>
2017-11-02 21:26:04 +0100

    Part 2

---
+++ Assets/Scripts/DiceRoller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRoller : MonoBehaviour
+{
+	public int[] diceValues;
+	public int diceTotal;
+
+	void Start ()
+	{
+		diceValues = new int[4];
+	}
+
+	void Update ()
+	{
+		
+	}
+
+	public void RollTheDice ()
+	{
+		diceTotal = 0;
+		for (int i = 0; i < diceValues.Length; i++) {
+			diceValues [i] = Random.Range (0, 2);
+			diceTotal += diceValues [i];
+			Debug.Log ("Rolled " + diceValues [i] + "(" + diceTotal + ")");
+		}
+	}
+}
